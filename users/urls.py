bbf9697ab9a766180947bae9074267b845a0b972
Donatz03 <127329064+Donatz03@users.noreply.github.com>
2025-06-28 14:44:12 +0200

    Add user deletion API view with permission checks

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CustomAuthToken, UserBanAPIView, UserMeAPIView
+from .views import CustomAuthToken, UserBanAPIView, UserMeAPIView, UserDeleteAPIView
 
 urlpatterns = [
     path('login/', CustomAuthToken.as_view(), name='api_token_auth'),
     path('ban/<int:pk>/', UserBanAPIView.as_view(), name='ban-user'),
     path('me/', UserMeAPIView.as_view(), name='user-me'),
+    path('<int:pk>/delete/', UserDeleteAPIView.as_view(), name='user-delete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics, permissions
-
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 
 from users.models import CustomUser
 from users.permissions import IsModerator
@@ -34,3 +34,14 @@ class UserMeAPIView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class UserDeleteAPIView(generics.DestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated, IsModerator]
+
+    def perform_destroy(self, instance):
+        if self.request.user == instance:
+            raise PermissionDenied("Non puoi eliminare te stesso.")
+        instance.delete()
