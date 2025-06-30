d1ed8d99f1f9abed3d2a9d0dd661d09513580c77
Donatz03 <127329064+Donatz03@users.noreply.github.com>
2025-06-30 15:55:05 +0200

    Add build and install phases to nixpacks.toml and configure STATIC_ROOT in settings.py

--- config/settings.py
+++ config/settings.py
@@ -173,11 +173,9 @@ CORS_ALLOW_HEADERS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-if 'RAILWAY_STATIC_URL' in os.environ:
-    STATIC_URL = os.environ['RAILWAY_STATIC_URL']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
